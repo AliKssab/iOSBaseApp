7f4d4e828c89e890fc86781bb9989cdb95fbe0e2
Ali Motafa <ali.mostafa.kassab2022@gmail.com>
2026-04-09 11:57:08 +0200

    Initial Commit

---
+++ iOSBaseApp2/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  iOSBaseApp2
+//
+//  Created by Ali Mostafa on 09/04/2026.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ iOSBaseApp2/iOSBaseApp2App.swift
@@ -0,0 +1,17 @@
+//
+//  iOSBaseApp2App.swift
+//  iOSBaseApp2
+//
+//  Created by Ali Mostafa on 09/04/2026.
+//
+
+import SwiftUI
+
+@main
+struct iOSBaseApp2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
